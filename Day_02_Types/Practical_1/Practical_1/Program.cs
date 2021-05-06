b24920fcb10ed32e162d3b26b739bf7543658404
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2021-05-06 16:11:02 +0400

    Practical 1 & 2 completed

---
+++ Day_02_Types/Practical_1/Practical_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practical_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 50, b = 100;
+
+            // აქ გამოვიყენე სტრინგის ინტერპოლაცია, კონკატენაციის გამოყენებაც შეიძლებოდა
+            Console.WriteLine($"Variable values before swap: a -> {a}, b -> {b}");
+
+            // ვარიანტი - ა, მესამე ცვლადის გამოყენებით
+            
+            int temp = a;
+            a = b;
+            b = temp;
+            
+            Console.WriteLine($"Variable values after swap: a -> {a}, b -> {b}");
+
+            // ვარიანტი - ბ, მესამე ცვლადის გამოყენების გარეშე
+
+            a = 50;
+            b = 100;
+
+            (a, b) = (b, a);
+
+            Console.WriteLine($"Variable values after swap: a -> {a}, b -> {b}");
+
+        }
+    }
+}
---
+++ Day_02_Types/Practical_2/Practical_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practical_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter year");
+            string inputYear = Console.ReadLine();
+            int year = Convert.ToInt32(inputYear);
+
+            // !(მარტივი წელიწადი) === ნაკიანი წელიწადი
+            bool commonYear = year % 4 != 0 || (year % 100 == 0 && year % 400 != 0);
+            Console.WriteLine(!commonYear);
+        }
+    }
+}
