bec5b69982ba9d0686ddf3fb18fe07894b53eb30
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2021-05-13 02:21:33 +0400

    Day_05 completed

---
+++ Day_05_Arrays/Practical_6/Practical_6/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Practical_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array row size: ");
+            int rowSize = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter array column size: ");
+            int columnSize = int.Parse(Console.ReadLine());
+
+            int[,] firstArray = new int[rowSize, columnSize];
+            int[,] secondArray = new int[rowSize, columnSize];
+
+
+            Console.WriteLine("Fill first matrix");
+
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < columnSize; j++)
+                {
+                    Console.Write($"Enter number for index {i},{j}: ");
+                    firstArray[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Fill second matrix");
+
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < columnSize; j++)
+                {
+                    Console.Write($"Enter number for index {i},{j}: ");
+                    secondArray[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Here is sum of two matrices");
+
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < columnSize; j++)
+                {
+                    Console.Write(firstArray[i, j] + secondArray[i, j]);
+                    if (j != columnSize - 1) Console.Write(", ");
+                }
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
---
+++ Day_05_Arrays/Practical_8/Practical_8/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace Practical_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ორგანზომილებიანი მასივით
+
+            Console.WriteLine("Using two dimensional array: ");
+
+            int[,] arr2d = new int[8, 8]
+            {
+                { 0, 1, 1, 1, 1, 1, 1, 1 },
+                { 0, 0, 1, 1, 1, 1, 1, 1 },
+                { 0, 0, 0, 1, 1, 1, 1, 1 },
+                { 0, 0, 0, 0, 1, 1, 1, 1 },
+                { 0, 0, 0, 0, 0, 1, 1, 1 },
+                { 0, 0, 0, 0, 0, 0, 1, 1 },
+                { 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+            };
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(arr2d[i, j]);
+                    if (j != 7) Console.Write(", ");
+                }
+                Console.Write("\n");
+            }
+
+
+            // ამ ციკლით დიაგონალურად შევატრიალე
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    int temp = arr2d[i, j];
+                    arr2d[i, j] = arr2d[j, i];
+                    arr2d[j, i] = temp;
+                }
+            }
+
+            // ამ ციკლით კი სარკისებურად
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 4; j++) // 8/2 გამომდინარეობს j < 4
+                {
+                    int temp = arr2d[i, j];
+                    arr2d[i, j] = arr2d[i, 7 - j]; // 7 - მაქს.ინდექსი
+                    arr2d[i, 7 - j] = temp;
+                }
+            }
+
+            Console.WriteLine("================================Separator line=======================================");
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(arr2d[i, j]);
+                    if (j != 7) Console.Write(", ");
+                }
+                Console.Write("\n");
+            }
+
+
+            // Jagged array-ით
+
+            Console.WriteLine("\nUsing jagged array ");
+
+            int[][] jArray = new int[8][]
+            {
+                new int[8] { 0, 1, 1, 1, 1, 1, 1, 1 },
+                new int[8] { 0, 0, 1, 1, 1, 1, 1, 1 },
+                new int[8] { 0, 0, 0, 1, 1, 1, 1, 1 },
+                new int[8] { 0, 0, 0, 0, 1, 1, 1, 1 },
+                new int[8] { 0, 0, 0, 0, 0, 1, 1, 1 },
+                new int[8] { 0, 0, 0, 0, 0, 0, 1, 1 },
+                new int[8] { 0, 0, 0, 0, 0, 0, 0, 1 },
+                new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 },
+            };
+
+
+            // ქვემოთ ციკლებში ლოგიკა იგივეა რაც 2დ მასივზე
+            
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(jArray[i][j]);
+                    if (j != 7) Console.Write(", ");
+                }
+                Console.Write("\n");
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    int temp = jArray[i][j];
+                    jArray[i][j] = jArray[j][i];
+                    jArray[j][i] = temp;
+                }
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    int temp = jArray[i][j];
+                    jArray[i][j] = jArray[i][7 - j];
+                    jArray[i][7 - j] = temp;
+                }
+            }
+
+            Console.WriteLine("================================Separator line=======================================");
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(jArray[i][j]);
+                    if (j != 7) Console.Write(", ");
+                }
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
