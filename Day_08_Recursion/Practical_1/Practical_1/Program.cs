f4aac942bd8030799e169152ae34bb99576111e5
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2021-05-20 16:08:47 +0400

    day-08 finished

---
+++ Day_08_Recursion/Practical_1/Practical_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practical_1
+{
+    class Program
+    {
+        static void PrintRange(int num)
+        {
+            if (num == 1)
+            {
+                Console.Write(1 + " ");
+            }
+            else
+            {
+                PrintRange(num - 1);
+                Console.Write(num + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrintRange(12);
+            Console.Write("\n");
+            PrintRange(15);
+        }
+    }
+}
---
+++ Day_08_Recursion/Practical_4/Practical_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practical_4
+{
+    class Program
+    {
+        static int RecursivePow(int num, int pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * RecursivePow(num, pow - 1);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            int threeCubed = RecursivePow(3, 3);
+            Console.WriteLine(threeCubed);
+        }
+    }
+}
