5f39ce415723231d4620685d81445ac761eaeaae
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2021-05-26 19:39:18 +0400

    day-10 finished

---
+++ Day_10_Reference_Types/Practical_1/Practical_1/Home.cs
@@ -0,0 +1,13 @@
+﻿namespace Practical_1
+{
+    public class Home
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public Home(string address, string city)
+        {
+            Address = address;
+            City = city;
+        }
+    }
+}
