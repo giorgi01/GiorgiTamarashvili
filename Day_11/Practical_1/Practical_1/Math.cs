db0453c3fefd4297de17555d8cd7647172d81498
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2021-05-31 13:23:25 +0400

    day-11 finished

---
+++ Day_11/Practical_1/Practical_1/Math.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practical_1
+{
+    public static class Math
+    {
+        public static double Pow(double num, double degree, out Status status)
+        {
+            if (degree < 0)
+            {
+                status = Status.PowMustBeaPositiveOrZero;
+                return (double)Status.PowMustBeaPositiveOrZero;
+            }
+            else if (degree == 0)
+            {
+                status = Status.Success;
+                return 1;
+            }
+            else
+            {
+                double result = num;
+                for (int i = 0; i < degree - 1; i++)
+                {
+                    result *= num;
+                }
+
+                status = Status.Success;
+                return result;
+            }
+        }
+
+        public static double Min(double a, double b, out Status status)
+        {
+            if (a < b)
+            {
+                status = Status.Success;
+                return a;
+            }
+            else if (a > b)
+            {
+                status = Status.Success;
+                return b;
+            }
+            else
+            {
+                status = Status.ValuesAreEqual;
+                return a;
+            }
+        }
+
+        public static double Max(double a, double b, out Status status)
+        {
+            if (a > b)
+            {
+                status = Status.Success;
+                return a;
+            }
+            else if (a < b)
+            {
+                status = Status.Success;
+                return b;
+            }
+            else
+            {
+                status = Status.ValuesAreEqual;
+                return a;
+            }
+        }
+
+    }
+}
---
+++ Day_11/Practical_1/Practical_1/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practical_1
+{
+    public enum Status
+    {
+        ValuesAreEqual = -1,
+        PowMustBeaPositiveOrZero,
+        Success
+    }
+}
