0cfb3369eca4c83b94fcbb0d728660d460dd7ccb
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2021-06-05 18:11:00 +0400

    day-15

---
+++ Day_15/Practical_1/Practical_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Practical_1
+{
+    class Program
+    {
+        static Matrix2D BuildMatrix2D()
+        {
+            Matrix2D matrix = new Matrix2D();
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write($"Enter number for index {i},{j}: ");
+                    matrix[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+            return matrix;
+        }
+
+        static void Main(string[] args)
+        {
+            Matrix2D matrix1 = BuildMatrix2D();
+            Matrix2D matrix2 = BuildMatrix2D();
+            Console.WriteLine((matrix1 - matrix2).ToString());
+            Console.WriteLine((matrix1 + matrix2).ToString());
+            Console.WriteLine((-matrix1).ToString());
+            Console.WriteLine((matrix1 * matrix2).ToString());
+            Console.WriteLine((matrix1 * 3).ToString());
+            Console.WriteLine(matrix1.Equals(matrix2));
+        }
+    }
+}
