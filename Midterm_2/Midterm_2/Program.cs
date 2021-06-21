b6398e6525da26a7e334aadab4799eab8b1f3c4c
DimitriDondoladze <dimitridondoladze41@gmail.com>
2021-06-21 15:27:08 +0400

    upload midterm 2

---
+++ Midterm_2/Midterm_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Midterm_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var userOption = Menu.GetUserOption();
+                if (userOption == Option.StartTest)
+                {
+                    QuizManager.StartTest();
+                }
+                else if (userOption == Option.AddTest)
+                {
+                    QuizManager.AddTest();
+                }
+            }
+            catch (SecondCorrectAnswerException ex)
+            {
+                Console.WriteLine("kitkhvis damatebisas mokhda shecdoma: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("mokhda shecdoma:" + ex.Message);
+            }
+        }
+    }
+}
+
