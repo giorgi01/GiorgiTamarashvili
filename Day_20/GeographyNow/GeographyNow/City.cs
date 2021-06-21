237efe990d0a614ac94ffd7c1770d194b8aa428f
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2021-06-21 15:42:52 +0400

    day-20

---
+++ Day_20/GeographyNow/GeographyNow/City.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeographyNow
+{
+    public class City : GeographicEntity
+    {
+        public bool IsCapital { get; set; }
+        public Country Country { get; set; }
+
+        public City()
+        {
+        }
+
+        public City(string name, double area, int citizens, bool isCapital, Country country)
+        {
+            Name = name;
+            Area = area;
+            Citizens = citizens;
+            IsCapital = isCapital;
+            Country = country;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(base.ToString());
+            sb.Append(", ");
+            sb.Append(Country.Name);
+            if (IsCapital) sb.Append(" (Capital)");
+            return Convert.ToString(sb);
+        }
+    }
+}
